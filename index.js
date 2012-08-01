a50e48e1501f52987306733c38ceb196d2a9a95a
Max Ogden <mogden@gmail.com>
2012-07-31 20:00:12 -0700

    ignore stupid macosx temporary hidden folders

--- index.js
+++ index.js
@@ -34,6 +34,7 @@ module.exports = function (inStream) {
             var s = findit(tmpDir);
             var files = [];
             s.on('file', function (file) {
+                if (file.match(/__MACOSX/)) return;
                 if (file.match(/\.shp$|\.kml$/i)) files.push(file);
             });
             s.on('end', next.ok.bind(null, files));
